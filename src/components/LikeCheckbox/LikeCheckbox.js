23d050e52d36097cfb62016944657c518a32c215
HwangSettong <iamsehyun@gmail.com>
2024-08-18 05:22:28 +0900

    add: PostItem, PostList

--- src/components/LikeCheckbox/LikeCheckbox.js
+++ src/components/LikeCheckbox/LikeCheckbox.js
@@ -3,11 +3,12 @@ import Checkbox from "@mui/material/Checkbox";
 import FavoriteBorder from "@mui/icons-material/FavoriteBorder";
 import Favorite from "@mui/icons-material/Favorite";
 
-const LikeCheckbox = ({ onClick }) => {
+const LikeCheckbox = ({ onClick, checked }) => {
     return (
       <Checkbox
         icon={<FavoriteBorder />}
         checkedIcon={<Favorite />}
+        checked={checked}
         sx={{
           color: "red",
           "&.Mui-checked": {
---
+++ src/pages/Community/Main.jsx
@@ -0,0 +1,8 @@
+
+export default function Main(props) {
+    return(
+        <>
+        </>
+    );
+}
+
