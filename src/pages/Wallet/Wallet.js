76d2ff7a29ae107b8a8201171c9721c7fd6dae0b
neu99 <neu990112@gmail.com>
2024-08-18 03:43:11 +0900

    fix: conflict Wallet, App

--- src/pages/Wallet/Wallet.js
+++ src/pages/Wallet/Wallet.js
@@ -100,21 +100,26 @@ export default function Wallet(props) {
     }, []);
 
     return (
-        <div>
-            <div className="card_detail">
-                <Link to="/wallet/cardDetail?no=1">
-                    카드1 상세 테스트 페이지 이동하기
-                </Link>
-            </div>
+        <Box sx={{
+            minHeight: '100vh',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'space-between',
+            paddingBottom: '75px', //
+            position: 'relative', //
+            overflow: 'hidden', //
+        }}>
             <Box
                 sx={{
-                    minHeight: '100vh',
                     display: 'flex',
                     flexDirection: 'column',
-                    justifyContent: 'space-between',
-                    paddingBottom: '75px', //
-                    position: 'relative', //
-                    overflow: 'hidden', //
+                    alignItems: 'center',
+                    width: '90%',
+                    margin: '0 auto',
+                    gap: 2,
+                    paddingTop: '20px',
+                    flexGrow: 1,
+                    paddingBottom: '150px', // 하단 여백 추가
                 }}
             >
                 {data && data.map((cardData, index) => (
@@ -130,18 +135,8 @@ export default function Wallet(props) {
             }}>
                 <Registration
 
-
-                <Box
-                    sx={{
-                        position: 'absolute',
-                        bottom: 0,
-                        left: 0,
-                        width: '100%',
-                    }}
-                >
-                    <Registration />
-                </Box>
+                />
             </Box>
-        </div>
+        </Box>
     );
 }
