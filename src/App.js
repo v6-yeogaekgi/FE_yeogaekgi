99232f57616dbdd98e5a88aa8716f4468352de01
hyunjin9603 <hyunjin9603@gmail.com>
2024-08-21 15:37:52 +0900

    feat:musicplayer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, {useState} from "react";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Footer from './layout/Footer/Footer';
-import Header from './layout/Header/Header';
+import Footer from "./layout/Footer/Footer";
+import Header from "./layout/Header/Header";
 import Wallet from './pages/Wallet/Wallet';
 import HomePage from './pages/Home/Home';
 import Kiosk from './pages/Kiosk/Kiosk';
@@ -13,21 +13,22 @@ import Box from '@mui/material/Box';
 // import CardDetail from './pages/Wallet/CardDetail';
 import Post from './pages/Community/Post';
 import EditComment from './pages/Community/EditComment';
-import ImageDetail from './pages/Community/ImageDetail';
+import ImageDetail from "./pages/Community/ImageDetail";
 import TopUp from './pages/TopUp/TopUp';
+
 
 const PageLayout = ({ children, menuName }) => {
     return (
-        <Box
-            sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                minHeight: '100vh',
-                paddingBottom: '70px', // Footer 높이만큼 패딩 추가
-            }}
-        >
+        <Box sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            minHeight: '100vh',
+            paddingBottom: '70px', // Footer 높이만큼 패딩 추가
+        }}>
             <Header menuName={menuName} />
-            <Box sx={{ flexGrow: 1, overflowY: 'auto' }}>{children}</Box>
+            <Box sx={{ flexGrow: 1, overflowY: 'auto' }}>
+                {children}
+            </Box>
             <Footer />
         </Box>
     );
@@ -37,84 +38,18 @@ function App() {
     return (
         <Router>
             <Routes>
-                <Route
-                    path={'/'}
-                    element={
-                        <PageLayout menuName={'home'}>
-                            <HomePage />
-                        </PageLayout>
-                    }
-                />
-                <Route
-                    path={'/home'}
-                    element={
-                        <PageLayout menuName={'home'}>
-                            <HomePage />
-                        </PageLayout>
-                    }
-                />
-                <Route
-                    path={'/wallet'}
-                    element={
-                        <PageLayout menuName={'wallet'}>
-                            <Wallet />
-                        </PageLayout>
-                    }
-                />
-                <Route
-                    path={'/map'}
-                    element={
-                        <PageLayout menuName={'map'}>
-                            <Map />
-                        </PageLayout>
-                    }
-                />
-                <Route
-                    path={'/community'}
-                    element={
-                        <PageLayout menuName={'community'}>
-                            <Main />
-                        </PageLayout>
-                    }
-                />
-                <Route path={'/community/post'} element={<Post />} />
-                <Route
-                    path={'/community/comment/edit'}
-                    element={
-                        <PageLayout menuName={'comment edit'}>
-                            <EditComment />
-                        </PageLayout>
-                    }
-                />
-                <Route
-                    path={'/community/imageDetail'}
-                    element={<ImageDetail />}
-                />
-                <Route
-                    path={'/myPage'}
-                    element={
-                        <PageLayout menuName={'my page'}>
-                            <MyPage />
-                        </PageLayout>
-                    }
-                />
+                <Route path={'/'} element={<PageLayout menuName={'home'}><HomePage /></PageLayout>} />
+                <Route path={"/home"} element={<PageLayout menuName={'home'}><HomePage /></PageLayout>} />
+                <Route path={"/wallet"} element={<PageLayout menuName={'wallet'}><Wallet /></PageLayout>} />
+                <Route path={"/map"} element={<PageLayout menuName={'map'}><Map/></PageLayout>} />
+                <Route path={"/community"} element={<PageLayout menuName={'community'}><Main /></PageLayout>} />
+                <Route path={"/community/post"} element={<Post />}/>
+                <Route path={"/community/comment/edit"} element={<PageLayout menuName={'comment edit'}><EditComment /></PageLayout>} />
+                <Route path={"/community/imageDetail"} element={<ImageDetail />} />
+                <Route path={"/myPage"} element={<PageLayout menuName={'my page'}><MyPage /></PageLayout>} />
                 {/* <Route path={"/wallet/card-detail"} element={<PageLayout menuName={'detail'}><CardDetail/></PageLayout>} /> */}
-                <Route
-                    path={'/wallet/top-up'}
-                    element={
-                        <PageLayout menuName={'top-up'}>
-                            <TopUp />
-                        </PageLayout>
-                    }
-                />
-                <Route
-                    path={'/kiosk'}
-                    element={
-                        <PageLayout menuName={'kiosk'}>
-                            <Kiosk />
-                        </PageLayout>
-                    }
-                />
+                <Route path={"/wallet/top-up"} element={<PageLayout menuName={'top-up'}><TopUp/></PageLayout>}/>
+                <Route path={"/kiosk"} element={<PageLayout menuName={'kiosk'}><Kiosk /></PageLayout>} />
             </Routes>
         </Router>
     );
--- src/layout/Header/Header.js
+++ src/layout/Header/Header.js
@@ -1,12 +1,13 @@
 import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
-import "./style.css";
+import './style.css';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
-import { IconButton } from "@mui/material";
+import { IconButton } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
+import Music from '../../components/Music/music';
 
 const Header = ({ menuName }) => {
     const navigate = useNavigate();
@@ -16,8 +17,11 @@ const Header = ({ menuName }) => {
     };
 
     return (
-        <Box sx={{ flexGrow: 1, color: '000', className: 'header'}}>
-            <AppBar position="static" sx={{ backgroundColor: 'white', color: 'black' }}>
+        <Box sx={{ flexGrow: 1, color: '000', className: 'header' }}>
+            <AppBar
+                position="static"
+                sx={{ backgroundColor: 'white', color: 'black' }}
+            >
                 <Toolbar sx={{ justifyContent: 'space-between' }}>
                     <IconButton
                         onClick={handleBack}
@@ -35,11 +39,14 @@ const Header = ({ menuName }) => {
                     <Typography variant="h6" component="div">
                         {menuName}
                     </Typography>
+                    <Box sx={{ position: 'absolute', right: 0 }}>
+                        <Music />
+                    </Box>
                     <Box sx={{ width: 48 }} />
                 </Toolbar>
             </AppBar>
         </Box>
     );
-}
+};
 
 export default Header;
