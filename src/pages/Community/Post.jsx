b0028edd362233565e9114125ec7a03782e8c50e
HwangSettong <iamsehyun@gmail.com>
2024-08-26 15:07:48 +0900

    fix: community error

--- src/pages/Community/Post.jsx
+++ src/pages/Community/Post.jsx
@@ -156,7 +156,6 @@ const Post = () => {
     useEffect(() => {
         setComment([]);
         setIsDataLoaded(true);
-        getPostApi();
     }, []);
     useEffect(() => {
         getPostApi();
