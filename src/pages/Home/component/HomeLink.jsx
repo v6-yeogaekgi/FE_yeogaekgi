dca20893fbebb0c3d3068fd11879f4328aa43c48
kevinlee <105219462+kiv9908@users.noreply.github.com>
2024-08-28 17:39:22 +0900

    Fix : LinkComponent

--- src/pages/Home/component/HomeLink.jsx
+++ src/pages/Home/component/HomeLink.jsx
@@ -11,6 +11,8 @@ import {
 import LocalActivityRoundedIcon from '@mui/icons-material/LocalActivityRounded';
 import FoodBankRoundedIcon from '@mui/icons-material/FoodBankRounded';
 import PhonelinkRingRoundedIcon from '@mui/icons-material/PhonelinkRingRounded';
+import TabletAndroidIcon from '@mui/icons-material/TabletAndroid';
+import TouchAppIcon from '@mui/icons-material/TouchApp';
 
 const HomeCardItem = () => {
     return (
@@ -24,7 +26,7 @@ const HomeCardItem = () => {
                 padding: 2,
                 boxShadow: 'none',
                 borderRadius: 5,
-                // backgroundColor: 'transparent',
+                backgroundColor: 'transparent',
             }}
         >
             <Box
@@ -36,7 +38,27 @@ const HomeCardItem = () => {
                     textAlign: 'center',
                 }}
             >
-                <LocalActivityRoundedIcon sx={{ fontSize: 40 }} />{' '}
+                <Box
+                    sx={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                    }}
+                >
+                    <LocalActivityRoundedIcon
+                        sx={{ fontSize: 70, color: '#808080' }}
+                    />
+                    <Typography
+                        sx={{
+                            fontFamily: 'Noto Sans, sans-serif',
+                            color: '#808080',
+                            mt: 1.5,
+                        }}
+                    >
+                        Activities
+                    </Typography>
+                </Box>
             </Box>
             <Box
                 sx={{
@@ -47,7 +69,27 @@ const HomeCardItem = () => {
                     textAlign: 'center',
                 }}
             >
-                <FoodBankRoundedIcon sx={{ fontSize: 40 }} />{' '}
+                <Box
+                    sx={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                    }}
+                >
+                    <FoodBankRoundedIcon
+                        sx={{ fontSize: 70, color: '#808080' }}
+                    />
+                    <Typography
+                        sx={{
+                            fontFamily: 'Noto Sans, sans-serif',
+                            color: '#808080',
+                            mt: 1.5,
+                        }}
+                    >
+                        Delivery
+                    </Typography>
+                </Box>
             </Box>
             <Box
                 sx={{
@@ -58,7 +100,25 @@ const HomeCardItem = () => {
                     textAlign: 'center',
                 }}
             >
-                <PhonelinkRingRoundedIcon sx={{ fontSize: 40 }} />{' '}
+                <Box
+                    sx={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                    }}
+                >
+                    <TouchAppIcon sx={{ fontSize: 70, color: '#808080' }} />
+                    <Typography
+                        sx={{
+                            fontFamily: 'Noto Sans, sans-serif',
+                            color: '#808080',
+                            mt: 1.5,
+                        }}
+                    >
+                        Kiosk
+                    </Typography>
+                </Box>
             </Box>
         </Card>
     );
