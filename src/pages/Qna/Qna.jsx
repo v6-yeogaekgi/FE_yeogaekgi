33db6a04588ddf6f50dfdadc2d8c2bbecdfc1315
kevinlee <105219462+kiv9908@users.noreply.github.com>
2024-08-31 15:50:57 +0900

    Feat : Qna design

--- src/pages/Qna/Qna.jsx
+++ src/pages/Qna/Qna.jsx
@@ -1,12 +1,24 @@
 import React, { useState, useRef, useEffect, useMemo, useContext } from 'react';
+import { Box } from '@mui/material';
 
 import QnaList from './component/QnaList';
 
 const Qna = () => {
     return (
-        <>
+        <Box
+            sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                height: '100vh',
+                paddingBottom: '70px',
+                backgroundColor: '#f0f4f8',
+                alignItems: 'center',
+                overflow: 'auto',
+                maxHeight: 'calc(100vh - 70px)',
+            }}
+        >
             <QnaList />
-        </>
+        </Box>
     );
 };
 
--- src/pages/Qna/component/QnaItem.jsx
+++ src/pages/Qna/component/QnaItem.jsx
@@ -1,8 +1,39 @@
-const QnaItem = () => {
+import React from 'react';
+import ListItem from '@mui/material/ListItem';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import ListItemText from '@mui/material/ListItemText';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import HourglassEmptyIcon from '@mui/icons-material/HourglassEmpty';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import ListItemSecondaryAction from '@mui/material/ListItemSecondaryAction';
+
+const QnaItem = ({ title, qnaDate, status }) => {
     return (
-        <>
-            <div>QnaItem, QnaList 작업중...</div>
-        </>
+        <ListItem>
+            <ListItemAvatar>
+                {status ? (
+                    <CheckCircleIcon sx={{ color: '#4653f9' }} />
+                ) : (
+                    <HourglassEmptyIcon sx={{ color: '#FF69B4' }} />
+                )}
+            </ListItemAvatar>
+            <ListItemText
+                primary={title}
+                secondary={qnaDate}
+                sx={{
+                    '& .MuiListItemText-primary': {
+                        fontWeight: 'bold',
+                        fontFamily: 'NotoSans, sans-serif',
+                    },
+                    '& .MuiListItemText-secondary': {
+                        fontFamily: 'NotoSans, sans-serif',
+                    },
+                }}
+            />
+            <ListItemSecondaryAction>
+                <ChevronRightIcon sx={{ color: '#6E7072' }} />
+            </ListItemSecondaryAction>
+        </ListItem>
     );
 };
 
