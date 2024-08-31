895b48390bb92a649a3d0d57cce689de06733e51
kevinlee <105219462+kiv9908@users.noreply.github.com>
2024-08-31 15:03:29 +0900

    Feat : QnaItem, QnaList

---
+++ src/pages/Qna/Qna.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useRef, useEffect, useMemo, useContext } from 'react';
+
+import QnaList from './component/QnaList';
+
+const Qna = () => {
+    return (
+        <>
+            <QnaList />
+        </>
+    );
+};
+
+export default Qna;
